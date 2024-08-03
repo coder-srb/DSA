cc15f275dd0e38edfa54646aa3079a72357bf82a
coder-srb <149040354+coder-srb@users.noreply.github.com>
2024-08-03 23:10:15 +0530

    Add files via upload

---
+++ Arrays/src/o1_LargestElement.java
@@ -0,0 +1,30 @@
+// 31. Find the largest element of a 2D-Array.
+
+import java.util.Scanner;
+
+public class o1_LargestElement {
+
+    static int max(int arr[][]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[2][3];
+        System.out.println("enter the elements of array: ");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println("largest element in the array is: " + max(arr));
+    }
+}
